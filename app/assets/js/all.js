7f20f92eb5dfa87a89ccd1fa07a1c5e05f269185
Fred Chang <fred8196@gmail.com>
2021-08-11 22:56:25 +0800

    Update 2021-08-11T14:56:23.275Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-const replyBtn=document.querySelector(".replyBtn");
-const ckEditor=document.querySelector( '#editor' );
-
-replyBtn.addEventListener("click",function (e) {
-    ClassicEditor.create(ckEditor)
-})
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var replyBtn = document.querySelector(".replyBtn");
+var ckEditor = document.querySelector('#editor');
+replyBtn.addEventListener("click", function (e) {
+  ClassicEditor.create(ckEditor);
+});
+//# sourceMappingURL=all.js.map
